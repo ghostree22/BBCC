4357579afd9bd2dd0d7258b0ab689b56c5547ac2
ghostree22 <69428676+ghostree22@users.noreply.github.com>
2020-08-13 12:48:33 +0900

    Add files via upload

---
+++ images/dccon_list.js
@@ -0,0 +1,19 @@
+﻿dcConsData = [
+	{name:"소닉링없찐.gif", 	keywords:["링없찐"], 		tags:["소닉", "소닉매니아"]},
+	{name:"소닉결혼반지.png",	keywords:["결혼반지"],		tags:["소닉", "소닉매니아"]},
+	{name:"겉바속촉.gif",	keywords:["겉바속촉", "가로쉬"],	tags:["와우", "헬스크림"]},
+
+	{name:"놀자곰댄스.gif",	keywords:["놀자곰댄스", "나곰댄스"],	tags:["춤"]},
+
+	{name:"댕댕당당.gif",	keywords:["댕댕당당"],		tags:["고양이"]},
+	{name:"댕댕댄스.gif",	keywords:["댕댕댄스"],		tags:["고양이", "춤"]},
+	{name:"댕댕박수.gif",	keywords:["댕댕박수"],		tags:["고양이", "뽁"]},
+	{name:"댕댕뿡.gif",		keywords:["댕댕뿡"],		tags:["고양이", "방구", "방귀"]},
+	{name:"댕댕웃어.gif",	keywords:["댕댕웃어"],		tags:["고양이", "꼬마찌", "생쥐"]},
+
+	{name:"메밀댄스.gif",	keywords:["메밀댄스"],		tags:["댕댕", "춤"]},
+	{name:"메밀댄스2.gif",	keywords:["메밀댄스2"],		tags:["춤"]},
+	{name:"메밀러닝.gif",	keywords:["메밀러닝"],		tags:["도트"]},
+	{name:"메밀만엉흔.gif",	keywords:["메밀만엉흔", "만엉흔"],	tags:["링피트"]},
+	{name:"메밀제발.gif",	keywords:["메밀제발"],		tags:["댕댕"]},
+];
